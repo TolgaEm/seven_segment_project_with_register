670759a5e66685926a5696bd1cc2dc09785f3ebb
Tolga Em <52701412+TolgaEm@users.noreply.github.com>
2022-06-29 02:04:28 +0300

    Update led_init.c

--- Drivers/Modules/led_init.c
+++ Drivers/Modules/led_init.c
@@ -9,10 +9,10 @@
 
 void led_init (void){
 
-	RCC->AHB1ENR   |= 0X00000001; // port A clock enable
+	RCC->AHB1ENR   = 0X00000001; // port A clock enable
 
-	GPIOA->MODER   |= 0X00001555; // A0,A1,A2,A3,A4,A5,A6 set output
-	GPIOA->OTYPER  |= 0X00000000;
-	GPIOA->OSPEEDR |= 0X00003FFF; // clock speed set as very high
-	GPIOA->PUPDR   |= 0X00000000; // set no pull up-down
+	GPIOA->MODER   = 0X00001555; // A0,A1,A2,A3,A4,A5,A6 set output
+	GPIOA->OTYPER  = 0X00000000;
+	GPIOA->OSPEEDR = 0X00003FFF; // clock speed set as very high
+	GPIOA->PUPDR   = 0X00000000; // set no pull up-down
 }
